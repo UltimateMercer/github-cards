7beabe0db3a0b25e8092c0b7c69af1558bee34e2
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-10-23 20:48:20 -0300

    feat: added navigation and images

---
+++ src/lib/components/navigation-bar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import ModeToggle from './mode-toggle.svelte';
+</script>
+
+<nav
+	class="sticky top-0 z-50 bg-background/95 backdrop-blur supports-[backgrop-filter]:bg-background/60"
+>
+	<div class="flex h-16 justify-between items-center px-5">
+		<div class="flex items-center">
+			<a href="/">
+				<img
+					src="ultimate-logo-red.svg"
+					class="!max-w-none mx-auto w-[56px] h-[56px] hidden dark:block"
+					width="56"
+					height="56"
+					alt="Ultimate Mercer Logo"
+				/>
+			</a>
+			<a href="/">
+				<img
+					src="ultimate-logo-dark.svg"
+					class="!max-w-none mx-auto w-[56px] h-[56px] block dark:hidden"
+					width="56"
+					height="56"
+					alt="Ultimate Mercer Logo"
+				/>
+			</a>
+		</div>
+		<div class="ml-auto">
+			<ModeToggle />
+		</div>
+	</div>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,37 @@
 <script lang="ts">
-  import * as Avatar from "$lib/components/ui/avatar";
-  import { Badge } from "$lib/components/ui/badge";
-  import { Button } from "$lib/components/ui/button";
-  import * as HoverCard from "$lib/components/ui/hover-card";
-  import { Input } from "$lib/components/ui/input";
-  import { Skeleton } from "$lib/components/ui/skeleton";
- </script>
-  
- <Avatar.Root>
-  <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
-  <Avatar.Fallback>CN</Avatar.Fallback>
- </Avatar.Root>
+	import * as Avatar from '$lib/components/ui/avatar';
+	import { Badge } from '$lib/components/ui/badge';
+	import { Button } from '$lib/components/ui/button';
+	import * as HoverCard from '$lib/components/ui/hover-card';
+	import { Input } from '$lib/components/ui/input';
+	import { Skeleton } from '$lib/components/ui/skeleton';
+</script>
 
- <Badge variant="outline">Outline</Badge>
+<main class="container mx-auto px-5 sm:px-6 lg:px-8 py-8">
+	<div
+		class="group relative !border rounded-lg shadow-lg transition-all min-h-[120px] flex flex-row gap-5 md:flex-row bg-background/20 backdrop-blur-md backdrop-filter hover:shadow pt-6 px-5 pb-5 mb-2 hover:hover-card-dark hover:dark:hover-card-light"
+	>
+		<div
+			class="absolute -top-4 bg-background inline-flex items-center px-2 py-1.5 rounded font-medium tracking-wide leading-none text-black dark:text-white !border"
+		>
+			Github Data
+		</div>
+	</div>
+	<Avatar.Root>
+		<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+		<Avatar.Fallback>CN</Avatar.Fallback>
+	</Avatar.Root>
 
- <Button variant="outline">Button</Button>
+	<Badge variant="outline">Outline</Badge>
 
- <HoverCard.Root>
-  <HoverCard.Trigger>Hover</HoverCard.Trigger>
-  <HoverCard.Content>
-    SvelteKit - Web development, streamlined
-  </HoverCard.Content>
-</HoverCard.Root>
+	<Button variant="outline">Button</Button>
 
-<Input />
+	<HoverCard.Root>
+		<HoverCard.Trigger>Hover</HoverCard.Trigger>
+		<HoverCard.Content>SvelteKit - Web development, streamlined</HoverCard.Content>
+	</HoverCard.Root>
 
-<Skeleton class="h-[20px] w-[100px] rounded-full" />
+	<Input />
+
+	<Skeleton class="h-[20px] w-[100px] rounded-full" />
+</main>
