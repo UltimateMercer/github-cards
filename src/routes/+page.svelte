5e1cdbfdb79cfc9998770aa26f7b1b68fff3fb70
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-10-23 19:22:36 -0300

    feat: added components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import * as Avatar from "$lib/components/ui/avatar";
+  import { Badge } from "$lib/components/ui/badge";
+  import { Button } from "$lib/components/ui/button";
+  import * as HoverCard from "$lib/components/ui/hover-card";
+  import { Input } from "$lib/components/ui/input";
+  import { Skeleton } from "$lib/components/ui/skeleton";
+ </script>
+  
+ <Avatar.Root>
+  <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+  <Avatar.Fallback>CN</Avatar.Fallback>
+ </Avatar.Root>
+
+ <Badge variant="outline">Outline</Badge>
+
+ <Button variant="outline">Button</Button>
+
+ <HoverCard.Root>
+  <HoverCard.Trigger>Hover</HoverCard.Trigger>
+  <HoverCard.Content>
+    SvelteKit - Web development, streamlined
+  </HoverCard.Content>
+</HoverCard.Root>
+
+<Input />
+
+<Skeleton class="h-[20px] w-[100px] rounded-full" />
