e5ece84e0ed7805dc54041f2a516179e26bc37d0
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-10-23 20:46:58 -0300

    feat: added mode toggle and fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+	import NavigationBar from '$lib/components/navigation-bar.svelte';
 	import '../app.css';
 	let { children } = $props();
+	import { ModeWatcher } from 'mode-watcher';
 </script>
 
+<ModeWatcher />
+<!-- <slot /> -->
+<NavigationBar />
 {@render children()}
