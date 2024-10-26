2d3644a59e224984aba90308c6594446a5b72cd7
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-10-26 17:32:57 -0300

    feat: added query clients

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	import NavigationBar from '$lib/components/navigation-bar.svelte';
-	import '../app.css';
-	let { children } = $props();
 	import { ModeWatcher } from 'mode-watcher';
+	import '../app.css';
+
+	let { children } = $props();
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser
+			}
+		}
+	});
 </script>
 
-<ModeWatcher />
-<!-- <slot /> -->
-<NavigationBar />
-{@render children()}
+<QueryClientProvider client={queryClient}>
+	<ModeWatcher />
+	<NavigationBar />
+	{@render children()}
+</QueryClientProvider>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,71 +10,100 @@
 	import type { User } from '../types';
 	import PinnedRepos from '$lib/components/pinned-repos.svelte';
 	import AllRepos from '$lib/components/all-repos.svelte';
+
+	import { createQuery } from '@tanstack/svelte-query';
+	import { fetchGithubData } from '$lib/api';
 </script>
 
 <script>
-	export let data;
+	let username: string = '';
+	let timer: ReturnType<typeof setTimeout>;
+	const debounce = (value: string) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			username = value;
+		}, 1000);
+	};
+
+	console.log(username);
+
+	$: query = createQuery({
+		queryKey: ['data', username],
+		queryFn: async () => {
+			const { user, repos, contributions, totalContributions, pinnedRepos } =
+				await fetchGithubData(username);
+			return { user, repos, contributions, totalContributions, pinnedRepos };
+		},
+		enabled: !!username
+	});
+
+	console.log(query);
 </script>
 
-{#if data.user}
-	<h1>{data.user.name} (@{data.user.login})</h1>
-{/if}
 <main class="container mx-auto px-5 sm:px-6 lg:px-8 py-8">
-	<Input class="mb-5" placeholder="Input" />
-
 	<div
-		class="group relative !border rounded-lg shadow-lg transition-all min-h-[120px] flex flex-col gap-4 bg-background/20 backdrop-blur-md backdrop-filter hover:shadow pt-6 px-5 pb-8 mb-2"
+		class="relative !border rounded-lg shadow-lg transition-all bg-background/20 backdrop-blur-md backdrop-filter hover:shadow pt-6 px-5 pb-6 mb-2"
 	>
 		<div
 			class="absolute -top-4 bg-background inline-flex items-center px-2 py-1.5 rounded font-medium tracking-wide leading-none text-black dark:text-white !border"
 		>
-			Github Data
+			Insert here a github username
 		</div>
-		<div class="flex flex-row gap-5">
-			<div class="">
-				<img src={data.user.avatar_url} class="h-56 w-56 object-cover rounded-lg" alt="" />
+		<Input
+			type="text"
+			placeholder="Input"
+			on:keyup={(event) => debounce((event.target as HTMLInputElement).value)}
+		/>
+	</div>
+	<pre>{username}</pre>
+
+	<div>
+		{#if $query.isLoading}
+			<p>Loading...</p>
+		{:else if $query.isError}
+			<p>Error: {$query.error.message}</p>
+		{:else if $query.isSuccess}
+			<pre>{JSON.stringify($query.data, null, 2)}</pre>
+		{/if}
+	</div>
+
+	<!-- {#if data || !data}
+		<div
+			class="group relative !border rounded-lg shadow-lg transition-all min-h-[120px] flex flex-col gap-4 bg-background/20 backdrop-blur-md backdrop-filter hover:shadow pt-6 px-5 pb-8 mb-2"
+		>
+			<div
+				class="absolute -top-4 bg-background inline-flex items-center px-2 py-1.5 rounded font-medium tracking-wide leading-none text-black dark:text-white !border"
+			>
+				Github Data
 			</div>
-			<div class="flex-1">
-				<h1 class="text-2xl">{data.user.name}</h1>
-				<h3 class="text-lg">@{data.user.login}</h3>
-
-				<p>{data.user.bio}</p>
-				<!-- <p>Public repos: {data.user.public_repos}</p> -->
-				<p>Followers: {data.user.followers}</p>
-				<p>Following: {data.user.following}</p>
-
-				<!-- <div class="">
-					<pre>{JSON.stringify(data.user, null, 2)}</pre>
-				</div> -->
-			</div>
-		</div>
-
-		<GitHubContributions username={data.user.login} />
-		<div class="grid grid-cols-1 lg:grid-cols-8 gap-8">
-			<div class="lg:col-span-3 px-6 w-full relative">
-				<div class="lg:sticky lg:top-20">
-					<PinnedRepos username={data.user.login} />
+			<div class="flex flex-row gap-5">
+				<div class="">
+					<img src={data.user.avatar_url} class="h-56 w-56 object-cover rounded-lg" alt="" />
+				</div>
+				<div class="flex-1">
+					<h1 class="text-2xl">{data.user.name}</h1>
+					<h3 class="text-lg">@{data.user.login}</h3>
+					<p>{data.user.bio}</p>
+					<p>Followers: {data.user.followers}</p>
+					<p>Following: {data.user.following}</p>
 				</div>
 			</div>
-			<div class="lg:col-span-5 w-full">
-				<h2 class="text-2xl font-bold mb-6">All Repositories</h2>
-				<AllRepos repos={data.repos} />
+
+			<GitHubContributions
+				contributions={data.contributions}
+				totalContributions={data.totalContributions}
+			/>
+			<div class="grid grid-cols-1 lg:grid-cols-8 gap-8">
+				<div class="lg:col-span-3 px-6 w-full relative">
+					<div class="lg:sticky lg:top-20">
+						<PinnedRepos username={data.user?.login} />
+					</div>
+				</div>
+				<div class="lg:col-span-5 w-full">
+					<h2 class="text-2xl font-bold mb-6">All Repositories</h2>
+					<AllRepos repos={data.repos} />
+				</div>
 			</div>
 		</div>
-	</div>
-	<Avatar.Root>
-		<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
-		<Avatar.Fallback>CN</Avatar.Fallback>
-	</Avatar.Root>
-
-	<Badge variant="outline">Outline</Badge>
-
-	<Button variant="outline">Button</Button>
-
-	<HoverCard.Root>
-		<HoverCard.Trigger>Hover</HoverCard.Trigger>
-		<HoverCard.Content>SvelteKit - Web development, streamlined</HoverCard.Content>
-	</HoverCard.Root>
-
-	<Skeleton class="h-[20px] w-[100px] rounded-full" />
+	{/if} -->
 </main>
