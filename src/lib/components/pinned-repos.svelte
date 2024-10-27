15ce634c07f6c5044efc00731ba7729dd62c4aaf
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-10-27 00:21:53 -0300

    refactor: adapt to receive data via props

--- src/lib/components/pinned-repos.svelte
+++ src/lib/components/pinned-repos.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import type { PinnedRepo } from '../../types';
+	import type { PinnedRepo, StandardPinnedRepo } from '../../types';
 	import { fetchUserPinnedRepos, isValidGitHubUsername } from '$lib/github-api';
 	import { BookMarked, GitFork, Star, Calendar } from 'lucide-svelte';
 	import Badge from './ui/badge/badge.svelte';
@@ -11,7 +11,7 @@
 
 	export let username: string;
 
-	let pinnedRepos: PinnedRepo[] = [];
+	export let pinnedRepos: StandardPinnedRepo[] = [];
 	let isLoading = true;
 	let error: string | null = null;
 
@@ -24,11 +24,9 @@
 		error = null;
 
 		try {
-			pinnedRepos = await fetchUserPinnedRepos(username);
+			isLoading = false;
 		} catch (e) {
 			error = e instanceof Error ? e.message : 'An unknown error occurred';
-		} finally {
-			isLoading = false;
 		}
 	}
 
@@ -54,33 +52,35 @@
 						<div
 							class="absolute -top-4 bg-background inline-flex items-center px-2 py-1.5 rounded font-medium tracking-wide leading-none text-black dark:text-white !border"
 						>
-							{repo.primaryLanguage ? repo.primaryLanguage.name : 'N/A'}
+							{repo.language || 'N/A'}
 						</div>
 
 						<h3 class="text-xl font-semibold break-all mb-2">
 							{#if username === repo.owner.login}
 								{repo.name}
 							{:else}
-								{repo.nameWithOwner}
+								{repo.full_name}
 							{/if}
 						</h3>
 						<p class="text-gray-600 mb-4">{repo.description || 'No description provided.'}</p>
 						<div class="flex flex-col justify-between text-sm">
 							<span class="flex items-center text-base font-medium mb-2">
 								<Star class="w-6 h-6 mr-1.5 fill-yellow-500" color="#eab308" />
-								{repo.stargazerCount}
+								{repo.stargazers_count}
 							</span>
 							<span class="flex items-center text-base font-medium mb-2">
 								<GitFork class="w-6 h-6 mr-1.5" />
-								{repo.forkCount}
+								{repo.forks_count}
 							</span>
 							<span class="flex items-center text-base font-medium mb-2">
-								<Calendar class="w-6 h-6 mr-1.5" />{formatFullDate({ date: repo.createdAt })}
+								<Calendar class="w-6 h-6 mr-1.5" />{formatFullDate({ date: repo.created_at })}
 							</span>
 						</div>
 						<div class="flex justify-between items-center mt-4">
 							<Button variant="link">
-								<a href={repo.url} target="_blank" rel="noopener noreferrer"> View Repository </a>
+								<a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+									View Repository
+								</a>
 							</Button>
 						</div>
 						<div
@@ -88,7 +88,7 @@
 						>
 							<Avatar.Root class="h-6 w-6">
 								<Avatar.Image
-									src={repo.owner.avatarUrl}
+									src={repo.owner.avatar_url}
 									class="object-cover"
 									alt={`@${repo.owner.login}`}
 								/>
